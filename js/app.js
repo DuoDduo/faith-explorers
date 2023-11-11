f22a5117d6298ba915cf10444e0897dfc998cedf
maverick <pelubs2956@gmail.com>
2023-11-11 19:49:23 +0100

    css and html updated

--- js/app.js
+++ js/app.js
@@ -78,96 +78,13 @@ window.addEventListener('scroll', handleScroll);
 //   type();
 // });
 
-// hotcards scroll
-
-
-const cardWrapper = document.querySelector('.hotcards')
-const cardWrapperChildren = Array.from(cardWrapper.children)
-const widthToScroll = cardWrapper.children[0].offsetWidth
-const arrowPrev = document.querySelector('.leftbtn')
-const arrowNext = document.querySelector('.rightbtn')
-const cardBounding = cardWrapper.getBoundingClientRect()
-const column = Math.floor(cardWrapper.offsetWidth / (widthToScroll + 24))
-let currScroll = 0
-let initPos = 0
-let clicked = false
-
-cardWrapperChildren.slice(-column).reverse().forEach(item => {
-  cardWrapper.insertAdjacentHTML('afterbegin', item.outerHTML)
-})
-
-cardWrapperChildren.slice(0, column).forEach(item => {
-  cardWrapper.insertAdjacentHTML('beforeend', item.outerHTML)
-})
-
-const cardImageAndLink = cardWrapper.querySelectorAll('.hcard img, .hcard a')
-cardImageAndLink.forEach(item => {
-  item.setAttribute('draggable', false)
-})
-
-cardWrapper.classList.add('no-smooth')
-cardWrapper.scrollLeft = cardWrapper.offsetWidth
-cardWrapper.classList.remove('no-smooth')
-
-arrowPrev.onclick = function () {
-  cardWrapper.scrollLeft -= widthToScroll
-}
-
-arrowNext.onclick = function () {
-  cardWrapper.scrollLeft += widthToScroll
-}
-
-cardWrapper.onmousedown = function (e) {
-  cardWrapper.classList.add('grab')
-  initPos = e.clientX - cardBounding.left
-  currScroll = cardWrapper.scrollLeft
-  clicked = true
-}
-
-cardWrapper.onmousemove = function (e) {
-  if (clicked) {
-    const xPos = e.clientX - cardBounding.left
-    cardWrapper.scrollLeft = currScroll + -(xPos - initPos)
-  }
-}
-
-cardWrapper.onmouseup = mouseUpAndLeave
-cardWrapper.onmouseleave = mouseUpAndLeave
-
-function mouseUpAndLeave() {
-  cardWrapper.classList.remove('grab')
-  clicked = false
-}
-
-let autoScroll
-
-cardWrapper.onscroll = function () {
-  if (cardWrapper.scrollLeft === 0) {
-    cardWrapper.classList.add('no-smooth')
-    cardWrapper.scrollLeft = cardWrapper.scrollWidth - (2 * cardWrapper.offsetWidth)
-    cardWrapper.classList.remove('no-smooth')
-  } else if (cardWrapper.scrollLeft === cardWrapper.scrollWidth - cardWrapper.offsetWidth) {
-    cardWrapper.classList.add('no-smooth')
-    cardWrapper.scrollLeft = cardWrapper.offsetWidth
-    cardWrapper.classList.remove('no-smooth')
-  }
-
-  if (autoScroll) {
-    clearTimeout(autoScroll)
-  }
-
-  autoScroll = setTimeout(() => {
-    cardWrapper.classList.remove('no-smooth')
-    cardWrapper.scrollLeft += widthToScroll
-  }, 3000)
-}
 
 
 
 
 // navbar toggle 
 const navlist = document.querySelector('.navlist');
-const checkbox = document.querySelector('#checkbox');
+const checkbox = document.getElementById('checkbox');
 
 checkbox.addEventListener('change', function () {
   if (this.checked) {
